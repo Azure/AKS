45a186adbf2fb660e261fd16ee3dfdbbbd25427f
Keith Mattix II <keithmattix2@gmail.com>
2023-07-20 14:01:26 -0500

    Update examples/envoy-ghsa-jfxv-29pc-x22r/get-istio-version.sh
    Co-authored-by: Shashank Barsin <shashankbarsin@users.noreply.github.com>

--- examples/envoy-ghsa-jfxv-29pc-x22r/get-istio-version.sh
+++ examples/envoy-ghsa-jfxv-29pc-x22r/get-istio-version.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kubectl get deploy -naks-istio-system istiod-asm-1-17 -ojson | jq '.spec.template.spec.containers[].image'
+kubectl get deploy -n aks-istio-system istiod-asm-1-17 -o json | jq '.spec.template.spec.containers[].image'
