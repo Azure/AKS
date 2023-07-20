eb69a1da123d535298a370c4d601dc270375b628
Keith Mattix II <keithmattix2@gmail.com>
2023-07-20 14:01:30 -0500

    Update examples/envoy-ghsa-jfxv-29pc-x22r/get-osm-envoy-version.sh
    Co-authored-by: Shashank Barsin <shashankbarsin@users.noreply.github.com>

--- examples/envoy-ghsa-jfxv-29pc-x22r/get-osm-envoy-version.sh
+++ examples/envoy-ghsa-jfxv-29pc-x22r/get-osm-envoy-version.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kubectl get deploy -nkube-system osm-injector -ojson | jq '.spec.template.spec.containers[].env[] | select(.name == "OSM_DEFAULT_ENVOY_IMAGE")'
+kubectl get deploy -n kube-system osm-injector -o json | jq '.spec.template.spec.containers[].env[] | select(.name == "OSM_DEFAULT_ENVOY_IMAGE")'
